a8704b00f23dce262c19df1b661b209bec2f81bc
clin99 <artiferlly@gmail.com>
2018-11-16 16:06:22 -0600

    Update

--- verilog_driver.hpp
+++ verilog_driver.hpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <cstddef>
+#include <fstream>
 #include <experimental/filesystem>
 
 #include "verilog_scanner.hpp"
@@ -12,17 +13,46 @@ namespace verilog {
 
 class ParserVerilogInterface {
   public:
+    virtual ~ParserVerilogInterface(){
+      if(!_scanner) delete _scanner;
+      if(!_parser) delete _parser;
+    }
     virtual void add_module(std::string&) = 0;
     virtual void add_input(std::string&) = 0;
 
-    void read(const std::experimental::filesystem::path&){}
+    void read(const std::experimental::filesystem::path&); 
+
+  private:
+    VerilogScanner* _scanner {nullptr};
+    VerilogParser*  _parser {nullptr};
+
 };
+
+inline void ParserVerilogInterface::read(const std::experimental::filesystem::path& p){
+  if(! std::experimental::filesystem::exists(p)){
+    return ;
+  }
+
+  std::ifstream ifs(p);
+  if(!_scanner){
+    _scanner = new VerilogScanner(&ifs);
+  }
+  if(!_parser){
+    _parser = new VerilogParser(*_scanner, *this);
+  }
+  _parser->parse();
+}
 
 struct ParserVerilog : public ParserVerilogInterface {
   void add_module(std::string&){}
   void add_input(std::string&){}
    
 };
+
+
+
+
+
 
 } 
 #endif 
